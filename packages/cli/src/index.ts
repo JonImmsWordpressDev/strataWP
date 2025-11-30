fff8ec2803ab1fc30a828ceb18b885ea838b9208
Jon Imms <jon.imms@buildertrend.com>
2025-11-30 14:23:36 -0600

    Bump version to 0.2.0 for new feature release

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -14,7 +14,7 @@ const program = new Command()
 program
   .name('wp-forge')
   .description('⚒️  A modern WordPress theme framework')
-  .version('0.1.0')
+  .version('0.2.0')
 
 // Development server
 program
